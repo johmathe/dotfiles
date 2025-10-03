ffeb5a3635a6eb5533670f1f891e7d90e25c09ea
Johan Mathe <johan.mathe@gmail.com>
2025-10-03 22:33:27 +0000

    Update

--- tmux/load_color.sh
+++ tmux/load_color.sh
@@ -5,9 +5,9 @@ load=$(uptime | awk -F'load average:' '{print $2}' | awk '{print $1}' | sed 's/,
 
 # Use darker background colors for better eye comfort
 if (( $(echo "$load < 0.25" | bc -l) )); then
-    echo "#[fg=colour235,bg=colour22] $load "  # Dark green background with spaces
+    echo "#[fg=colour235,bg=colour22] $load"  # Dark green background, one space
 elif (( $(echo "$load < 0.75" | bc -l) )); then
-    echo "#[fg=colour235,bg=colour58] $load "  # Dark yellow background with spaces
+    echo "#[fg=colour235,bg=colour58] $load"  # Dark yellow background, one space
 else
-    echo "#[fg=colour235,bg=colour52] $load "  # Dark red background with spaces
+    echo "#[fg=colour235,bg=colour52] $load"  # Dark red background, one space
 fi
--- tmux/mem_usage.sh
+++ tmux/mem_usage.sh
@@ -5,9 +5,9 @@ mem=$(free | grep Mem | awk '{printf("%.1f", $3/$2 * 100.0)}')
 
 # Use darker background colors for better eye comfort
 if (( $(echo "$mem < 30" | bc -l) )); then
-    echo "#[fg=colour235,bg=colour22] $mem% "  # Dark green background
+    echo "#[fg=colour235,bg=colour22] $mem%"  # Dark green background, one space
 elif (( $(echo "$mem < 75" | bc -l) )); then
-    echo "#[fg=colour235,bg=colour58] $mem% "  # Dark yellow background
+    echo "#[fg=colour235,bg=colour58] $mem%"  # Dark yellow background, one space
 else
-    echo "#[fg=colour235,bg=colour52] $mem% "  # Dark red background
+    echo "#[fg=colour235,bg=colour52] $mem%"  # Dark red background, one space
 fi
